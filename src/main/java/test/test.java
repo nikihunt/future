c6bb50e9d252d8117f746431fe373c42b8ea0c3d
nikihunt <759640248@qq.com>
2016-02-25 18:38:05 +0800

    test2

--- src/main/java/test/test.java
+++ src/main/java/test/test.java
@@ -17,6 +17,5 @@ public class test extends HttpServlet {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        doPost(request,response);
     }
 }
